137c2491c5864ba734fa4b6ccfc1ee52d8f96555
NotJesus172 <157871959+NotJesus172@users.noreply.github.com>
2024-09-17 19:20:05 -0400

    09/17/24-1920

--- install.sh
+++ install.sh
@@ -11,20 +11,22 @@ sudo nala upgrade -y
 #   Install curl:
 sudo nala install -y curl
 
-#   Install and configure pihole:
+
 echo "Would you like to begin the PiHole installation? (y/n)"
 read readyState
 if [ $readyState == "y" ]; then
     echo "Very well, the PiHole install will begin now."
-    sudo ./config/pihole.sh
 elif [ $readyState == "Y" ]; then
     echo "Very well, the PiHole install will begin now."
-    sudo ./config/pihole.sh
 else
     exit 1
 fi
 
+#   Install and configure pihole:
+sudo ./config/pihole.sh
 
-
-#   
+#   Set the fresh pihole password:
 sudo ./config/setPasswd.sh
+
+#   Install and configure unbound:
+sudo ./config/unbound.sh
