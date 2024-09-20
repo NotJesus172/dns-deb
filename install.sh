5a938102bad6b7eb64ddc1ec8ca5d5445cb4445c
NotJesus172 <157871959+NotJesus172@users.noreply.github.com>
2024-09-20 00:47:37 -0400

    1.0.1

--- install.sh
+++ install.sh
@@ -8,17 +8,15 @@ sudo nala update
 sudo nala upgrade -y
 #   Install curl:
 sudo nala install -y curl
-#    Make all nested scripts executable:
+#   Make all nested scripts executable:
 sudo chmod +x ./config/pihole.sh
 sudo chmod +x ./config/setPasswd.sh
 sudo chmod +x ./config/unbound.sh
 sudo chmod +x ./gravity-sync.sh
-#    Check in to make sure you want to install pihole:
+#   Check in to make sure you want to install pihole:
 echo "Would you like to begin the PiHole installation? (y/n)"
 read readyState
-if [ $readyState == "y" ]; then
-    echo "Very well, the PiHole install will begin now."
-elif [ $readyState == "Y" ]; then
+if [[ $readyState == "y" || $readyState == "Y" ]]; then
     echo "Very well, the PiHole install will begin now."
 else
     exit 1
@@ -29,17 +27,15 @@ sudo ./config/pihole.sh
 sudo ./config/setPasswd.sh
 #   Install and configure unbound:
 sudo ./config/unbound.sh
-#    Check in to make sure you want to install gravity-sync:
+#   Check in to make sure you want to install gravity-sync:
 echo "Would you like to install Gravity-Sync? (y/n)"
 read syncState
-if [ $syncState == "y" ]; then
+if [[ $syncState == "y" || $syncState == "Y" ]]; then
     echo "Very well, the Gravity-Sync install will begin now."
-elif [ $syncState == "Y" ]; then
-    echo "Very well, the Gravity-Sync install will begin now."
+    #   Install  gravity sync:
+    sudo ./gravity-sync.sh
 else
+    #   Future install instructions:
     echo "Gravity-Sync will NOT be installed now."
-    echo "If you would like to enable it at a later time return to this directory and run './gravity-sync.sh' to begin the installer."
-    exit 1
+    echo "If you would like to Install it at a later time, return to this directory and run './gravity-sync.sh' to begin the installer."
 fi
-#    Install  gravity sync:
-sudo ./gravity-sync.sh
